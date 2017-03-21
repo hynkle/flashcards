4626fc36250157b3ecde64a9aa0551e3183cc7f7
Spencer Ruiz and Sara Schultz <spencer@>
2017-03-21 13:58:01 -0700

    add dictionary array with all concept titles and create random generator.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
 
-
-  var conceptTitle = "JavaScript";
-  var conceptBody = "This is text.";
-
   $("#conceptPanel").click(function() {
     $("#definitionPanel").show();
   });
@@ -13,10 +9,16 @@ $(document).ready(function() {
   });
 
   $("#nextButton").click(function() {
-    $("#conceptName").empty(conceptTitle);
-    $("#conceptDef").empty(conceptBody);
-    $("#conceptName").prepend(conceptTitle);
-    $("#conceptDef").prepend(conceptBody);
+    $("#conceptName").empty();
+    $("#conceptDef").empty();
+    var randomNumber = Math.floor((Math.random() * 18));
+    $("#conceptName").prepend(dictionary[randomNumber][0]);
+    $("#conceptDef").prepend(dictionary[randomNumber][1]);
   });
 
 });
+
+var conceptTitle = "JavaScript";
+var conceptBody = "This is text.";
+
+var dictionary = [["JavaScipt", "This is text."],["Operators", "This is text."], ["Variables", "This is text."],["Variable Naming Conventions", "This is text."],["Functions", "This is text."],["Methods", "This is text."],["Arguments", "This is text."],["Parameters", "This is text."],["Return", "This is text."],["Chaining Methods", "This is text."],["Strings", "This is text."],["Booleans", "This is text."],["Undefined", "This is text."],["NaN", "This is text."],["Escape", "This is text."],["Alert", "This is text."],["Comments", "This is text."],["jQuery", "This is text."],["Attributes", "This is text."]]
